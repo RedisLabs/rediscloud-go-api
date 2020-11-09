5e5561ac63567743425984be81b0b2069bc89cc8
Will May <will.j.may@gmail.com>
2020-11-09 10:55:15 +0000

    Support task eventual consistency
    It is possible for the task API to return 404 for a newly created task, so allow two 404 responses for a task before giving up on the third one.

--- task_test.go
+++ task_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTaskErrorsGetUnwraped(t *testing.T) {
+func TestTaskErrorsGetUnwrapped(t *testing.T) {
 	s := httptest.NewServer(testServer("key", "secret", deleteRequest(t, "/cloud-accounts/1", `{
   "taskId": "task",
   "commandType": "cloudAccountDeleteRequest",
@@ -55,4 +55,41 @@ func TestTaskErrorsGetUnwraped(t *testing.T) {
 		Description: redis.String("Payment info was not found for subscription. Use 'GET /payment-methods' to lookup valid payment methods for current Account"),
 		Status:      redis.String("400 BAD_REQUEST"),
 	}, errors.Unwrap(err))
+}
+
+func TestTask_GivesUpAfterThree404s(t *testing.T) {
+	s := httptest.NewServer(testServer("key", "secret", deleteRequest(t, "/cloud-accounts/1", `{
+  "taskId": "task",
+  "commandType": "cloudAccountDeleteRequest",
+  "status": "received",
+  "description": "Task request received and is being queued for processing.",
+  "timestamp": "2020-11-02T09:05:34.3Z",
+  "_links": {
+    "task": {
+      "href": "https://example.org",
+      "title": "getTaskStatusUpdates",
+      "type": "GET"
+    }
+  }
+}`), getRequest(t, "/tasks/task", `{
+  "taskId": "e02b40d6-1395-4861-a3b9-ecf829d835fd",
+  "commandType": "cloudAccountDeleteRequest",
+  "status": "initialized",
+  "timestamp": "2020-10-28T09:58:16.798Z",
+  "response": {},
+  "_links": {
+    "self": {
+      "href": "https://example.com",
+      "type": "GET"
+    }
+  }
+}`), getRequestWithStatus(t, "/tasks/task", 404, ""),
+		getRequestWithStatus(t, "/tasks/task", 404, ""),
+		getRequestWithStatus(t, "/tasks/task", 404, "")))
+
+	subject, err := clientFromTestServer(s, "key", "secret")
+	require.NoError(t, err)
+
+	err = subject.CloudAccount.Delete(context.TODO(), 1)
+	assert.Error(t, err)
 }
