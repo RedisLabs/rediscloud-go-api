b41c217b6cece184ad37f2f2a65cb70118669810
Peter Vegh <peter.vegh@opencredo.com>
2022-12-15 09:51:58 +0000

    Regions API calls and structs

--- service/regions/service.go
+++ service/regions/service.go
@@ -3,6 +3,9 @@ package regions
 import (
 	"context"
 	"fmt"
+	"net/http"
+
+	"github.com/RedisLabs/rediscloud-go-api/internal"
 )
 
 type Log interface {
@@ -13,7 +16,7 @@ type HttpClient interface {
 	Get(ctx context.Context, name, path string, responseBody interface{}) error
 	Post(ctx context.Context, name, path string, requestBody interface{}, responseBody interface{}) error
 	Put(ctx context.Context, name, path string, requestBody interface{}, responseBody interface{}) error
-	Delete(ctx context.Context, name, path string, responseBody interface{}) error
+	DeleteWithQuery(ctx context.Context, name, path string, requestBody interface{}, responseBody interface{}) error
 }
 
 type Task interface {
@@ -32,6 +35,24 @@ func NewAPI(client HttpClient, task Task, logger Log) *API {
 	return &API{client: client, task: task, logger: logger}
 }
 
+// Create will create a new subscription.
+func (a *API) Create(ctx context.Context, subId int, region CreateRegion) (int, error) {
+	var task taskResponse
+	err := a.client.Post(ctx, "create subscription", fmt.Sprintf("/subscriptions/%d/regions", subId), region, &task)
+	if err != nil {
+		return 0, err
+	}
+
+	a.logger.Printf("Waiting for task %s to finish creating the subscription", task)
+
+	id, err := a.task.WaitForResourceId(ctx, *task.ID)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
 // List will list all of a given subscription's active-active regions.
 func (a API) List(ctx context.Context, subId int) (*Regions, error) {
 	var response Regions
@@ -41,4 +62,36 @@ func (a API) List(ctx context.Context, subId int) (*Regions, error) {
 	}
 
 	return &response, nil
+}
+
+func (a *API) DeleteWithQuery(ctx context.Context, id int, regions DeleteRegions) error {
+	var task taskResponse
+	err := a.client.DeleteWithQuery(ctx, fmt.Sprintf("delete region %d", id), fmt.Sprintf("/subscriptions/%d/regions/", id), regions, &task)
+	if err != nil {
+		return err
+	}
+
+	a.logger.Printf("Waiting for region %d to finish being deleted", id)
+
+	err = a.task.Wait(ctx, *task.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type NotFound struct {
+	id int
+}
+
+func (f *NotFound) Error() string {
+	return fmt.Sprintf("subscription %d not found", f.id)
+}
+
+func wrap404Error(id int, err error) error {
+	if v, ok := err.(*internal.HTTPError); ok && v.StatusCode == http.StatusNotFound {
+		return &NotFound{id: id}
+	}
+	return err
 }
