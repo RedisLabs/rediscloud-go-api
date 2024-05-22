5b88d15fe0ef44d5f5dda5cf771485d997dd69bc
John Sharpe <JohnSharpe@users.noreply.github.com>
2024-05-22 10:27:43 +0100

    Explicit latest responses (#165)
    * Getting a detailed import status request (yet to do failureReasonParams and backups)
    
    * Included failureReasonParams
    
    * Added backup resource detail
    
    * Upped some github actions versions

--- latest_imports_test.go
+++ latest_imports_test.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/RedisLabs/rediscloud-go-api/redis"
+	"github.com/RedisLabs/rediscloud-go-api/service/latest_imports"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/stretchr/testify/require"
 )
@@ -27,6 +32,31 @@ func TestGetLatestImportTooEarly(t *testing.T) {
 					  "href": "https://api-staging.qa.redislabs.com/v1/tasks/1dfd6084-21df-40c6-829c-e9b4790e207e",
 					  "type": "GET",
 					  "rel": "task"
+					}
+				  ]
+				}`,
+			),
+			getRequest(
+				t,
+				"/tasks/1dfd6084-21df-40c6-829c-e9b4790e207e",
+				`{
+				  "taskId": "1dfd6084-21df-40c6-829c-e9b4790e207e",
+				  "commandType": "databaseImportStatusRequest",
+				  "status": "processing-error",
+				  "description": "Task request failed during processing. See error information for failure details.",
+				  "timestamp": "2024-04-15T10:19:07.331898Z",
+				  "response": {
+					"error": {
+					  "type": "SUBSCRIPTION_NOT_ACTIVE",
+					  "status": "403 FORBIDDEN",
+					  "description": "Cannot preform any actions for subscription that is not in an active state"
+					}
+				  },
+				  "links": [
+					{
+					  "href": "https://api-staging.qa.redislabs.com/v1/tasks/1dfd6084-21df-40c6-829c-e9b4790e207e",
+					  "type": "GET",
+					  "rel": "self"
 					}
 				  ]
 				}`,
@@ -100,7 +130,34 @@ func TestGetFixedLatestImport(t *testing.T) {
 				  "response": {
 					"resourceId": 51051302,
 					"additionalResourceId": 110777,
-					"resource": {}
+					"resource": {
+                      "status": "importing"
+                	}
+				  },
+				  "links": [
+					{
+					  "href": "https://api-staging.qa.redislabs.com/v1/tasks/e9232e43-3781-4263-a38e-f4d150e03475",
+					  "type": "GET",
+					  "rel": "self"
+					}
+				  ]
+				}`,
+			),
+			getRequest(
+				t,
+				"/tasks/e9232e43-3781-4263-a38e-f4d150e03475",
+				`{
+				  "taskId": "e9232e43-3781-4263-a38e-f4d150e03475",
+				  "commandType": "databaseImportStatusRequest",
+				  "status": "processing-completed",
+				  "description": "Request processing completed successfully and its resources are now being provisioned / de-provisioned.",
+				  "timestamp": "2024-04-15T10:44:35.225468Z",
+				  "response": {
+					"resourceId": 51051302,
+					"additionalResourceId": 110777,
+					"resource": {
+                      "status": "importing"
+                	}
 				  },
 				  "links": [
 					{
@@ -116,8 +173,22 @@ func TestGetFixedLatestImport(t *testing.T) {
 	subject, err := clientFromTestServer(server, "key", "secret")
 	require.NoError(t, err)
 
-	_, err = subject.LatestImport.GetFixed(context.TODO(), 12, 34)
+	actual, err := subject.LatestImport.GetFixed(context.TODO(), 12, 34)
 	require.NoError(t, err)
+
+	assert.Equal(t, &latest_imports.LatestImportStatus{
+		CommandType: redis.String("databaseImportStatusRequest"),
+		Description: redis.String("Request processing completed successfully and its resources are now being provisioned / de-provisioned."),
+		Status:      redis.String("processing-completed"),
+		ID:          redis.String("e9232e43-3781-4263-a38e-f4d150e03475"),
+		Response: &latest_imports.Response{
+			ID: redis.Int(51051302),
+			Resource: &latest_imports.Resource{
+				Status: redis.String("importing"),
+			},
+			Error: nil,
+		},
+	}, actual)
 }
 
 func TestGetLatestImport(t *testing.T) {
@@ -155,7 +226,58 @@ func TestGetLatestImport(t *testing.T) {
 				  "response": {
 					"resourceId": 51051302,
 					"additionalResourceId": 110777,
-					"resource": {}
+					"resource": {
+					  "failureReason": "file-corrupted",
+					  "failureReasonParams": [
+						{
+						  "key": "bytes_configured_bdb_limit",
+						  "value": "1234"
+						},
+						{
+						  "key": "bytes_of_expected_dataset",
+						  "value": "5678"
+						}
+					  ],
+					  "lastImportTime": "2024-05-21T10:36:26Z",
+                      "status": "failed"
+					}
+				  },
+				  "links": [
+					{
+					  "href": "https://api-staging.qa.redislabs.com/v1/tasks/e9232e43-3781-4263-a38e-f4d150e03475",
+					  "type": "GET",
+					  "rel": "self"
+					}
+				  ]
+				}`,
+			),
+			getRequest(
+				t,
+				"/tasks/e9232e43-3781-4263-a38e-f4d150e03475",
+				`{
+				  "taskId": "e9232e43-3781-4263-a38e-f4d150e03475",
+				  "commandType": "databaseImportStatusRequest",
+				  "status": "processing-completed",
+				  "description": "Request processing completed successfully and its resources are now being provisioned / de-provisioned.",
+				  "timestamp": "2024-04-15T10:44:35.225468Z",
+				  "response": {
+					"resourceId": 51051302,
+					"additionalResourceId": 110777,
+					"resource": {
+					  "failureReason": "file-corrupted",
+					  "failureReasonParams": [
+						{
+						  "key": "bytes_configured_bdb_limit",
+						  "value": "1234"
+						},
+						{
+						  "key": "bytes_of_expected_dataset",
+						  "value": "5678"
+						}
+					  ],
+					  "lastImportTime": "2024-05-21T10:36:26Z",
+                      "status": "failed"
+					}
 				  },
 				  "links": [
 					{
@@ -171,6 +293,32 @@ func TestGetLatestImport(t *testing.T) {
 	subject, err := clientFromTestServer(server, "key", "secret")
 	require.NoError(t, err)
 
-	_, err = subject.LatestImport.Get(context.TODO(), 12, 34)
+	actual, err := subject.LatestImport.Get(context.TODO(), 12, 34)
 	require.NoError(t, err)
+
+	assert.Equal(t, &latest_imports.LatestImportStatus{
+		CommandType: redis.String("databaseImportStatusRequest"),
+		Description: redis.String("Request processing completed successfully and its resources are now being provisioned / de-provisioned."),
+		Status:      redis.String("processing-completed"),
+		ID:          redis.String("e9232e43-3781-4263-a38e-f4d150e03475"),
+		Response: &latest_imports.Response{
+			ID: redis.Int(51051302),
+			Resource: &latest_imports.Resource{
+				Status:         redis.String("failed"),
+				LastImportTime: redis.Time(time.Date(2024, 5, 21, 10, 36, 26, 0, time.UTC)),
+				FailureReason:  redis.String("file-corrupted"),
+				FailureReasonParams: []*latest_imports.FailureReasonParam{
+					{
+						Key:   redis.String("bytes_configured_bdb_limit"),
+						Value: redis.String("1234"),
+					},
+					{
+						Key:   redis.String("bytes_of_expected_dataset"),
+						Value: redis.String("5678"),
+					},
+				},
+			},
+			Error: nil,
+		},
+	}, actual)
 }
--- service/latest_imports/model.go
+++ service/latest_imports/model.go
@@ -1,9 +1,9 @@
 package latest_imports
 
 import (
-	"encoding/json"
 	"fmt"
 	"regexp"
+	"time"
 
 	"github.com/RedisLabs/rediscloud-go-api/internal"
 	"github.com/RedisLabs/rediscloud-go-api/redis"
@@ -22,12 +22,32 @@ func (o LatestImportStatus) String() string {
 }
 
 type Response struct {
-	ID       *int             `json:"resourceId,omitempty"`
-	Resource *json.RawMessage `json:"resource,omitempty"`
-	Error    *Error           `json:"error,omitempty"`
+	ID       *int      `json:"resourceId,omitempty"`
+	Resource *Resource `json:"resource,omitempty"`
+	Error    *Error    `json:"error,omitempty"`
 }
 
 func (o Response) String() string {
+	return internal.ToString(o)
+}
+
+type Resource struct {
+	Status              *string               `json:"status,omitempty"`
+	LastImportTime      *time.Time            `json:"lastImportTime,omitempty"`
+	FailureReason       *string               `json:"failureReason,omitempty"`
+	FailureReasonParams []*FailureReasonParam `json:"failureReasonParams,omitempty"`
+}
+
+func (o Resource) String() string {
+	return internal.ToString(o)
+}
+
+type FailureReasonParam struct {
+	Key   *string `json:"key,omitempty"`
+	Value *string `json:"value,omitempty"`
+}
+
+func (o FailureReasonParam) String() string {
 	return internal.ToString(o)
 }
 
@@ -37,7 +57,7 @@ type Error struct {
 	Status      *string `json:"status,omitempty"`
 }
 
-func (e Error) String() string {
+func (e *Error) String() string {
 	return internal.ToString(e)
 }
 
@@ -54,36 +74,6 @@ func (e *Error) Error() string {
 }
 
 var errorStatusCode = regexp.MustCompile("^(\\d*).*$")
-
-func NewLatestImportStatus(task *internal.Task) *LatestImportStatus {
-	latestImportStatus := LatestImportStatus{
-		CommandType: task.CommandType,
-		Description: task.Description,
-		Status:      task.Status,
-		ID:          task.ID,
-	}
-
-	if task.Response != nil {
-		r := Response{
-			ID:       task.Response.ID,
-			Resource: task.Response.Resource,
-		}
-
-		if task.Response.Error != nil {
-			e := Error{
-				Type:        task.Response.Error.Type,
-				Description: task.Response.Error.Description,
-				Status:      task.Response.Error.Status,
-			}
-
-			r.Error = &e
-		}
-
-		latestImportStatus.Response = &r
-	}
-
-	return &latestImportStatus
-}
 
 type NotFound struct {
 	subId int
